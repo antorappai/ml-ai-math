6a369a2a95a0560c30a0a5acbb07b3c27bcb71fe
Antony Pulikkottil Rappai <60907051+antorappai@users.noreply.github.com>
2026-09-13 22:02:26 +0530

    Let NLP page frame each section consistently

--- src/content/nlpGuide.js
+++ src/content/nlpGuide.js
@@ -4,5 +4,5 @@ export const nlpIntroduction = source.split(/^## /m)[0];
 export const nlpSections = Array.from(source.matchAll(/^## (.+)\n([\s\S]*?)(?=^## |$(?![\s\S]))/gm), (match, index) => ({
   id: String(index + 1),
   title: match[1],
-  markdown: `## ${match[1]}\n${match[2]}`,
+  markdown: match[2],
 }));
